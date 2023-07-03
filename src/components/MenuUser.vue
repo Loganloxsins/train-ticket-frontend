d6fbe14a056dc18ed5fee3534aaba4c8eb832ad5
chenhongyu <1428467470@qq.com>
2023-07-03 17:49:05 +0800

    个人中心增加了积分和注册跳转，删掉了导航栏的两栏

--- src/components/MenuUser.vue
+++ src/components/MenuUser.vue
@@ -1,19 +1,19 @@
 <script lang="ts" setup>
 import { toggleDark } from '~/composables';
-import {useRouter} from "vue-router";
-import {useUserStore} from "~/stores/user";
-import {request} from "~/utils/request";
-import {AxiosError, AxiosResponse} from "axios";
-import {ElNotification} from "element-plus";
-import {h} from "vue";
+import { useRouter } from "vue-router";
+import { useUserStore } from "~/stores/user";
+import { request } from "~/utils/request";
+import { AxiosError, AxiosResponse } from "axios";
+import { ElNotification } from "element-plus";
+import { h } from "vue";
 defineProps<{
-    pageIndex: string
+  pageIndex: string
 }>()
 
 const router = useRouter()
 const user = useUserStore()
 const menuSelect = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath);
+  console.log(key, keyPath);
 }
 
 const logout = () => {
@@ -41,24 +41,24 @@ const logout = () => {
 </script>
         
 <template>
-    <el-menu :default-active="pageIndex" mode="horizontal" class="el-menu-demo" @select="menuSelect" :router=true>
-<!--        <el-menu-item index="/">主页</el-menu-item>-->
-        <el-menu-item index="/userhome">你好乘客</el-menu-item>
-        <el-menu-item index="/search">车票购买</el-menu-item>
-        <el-menu-item index="/vipregister">会员注册中心</el-menu-item>
-        <el-menu-item index="/point">积分查询</el-menu-item>
-        <el-menu-item index="/admin">切换为管理员页面</el-menu-item>
-        <el-menu-item h="full" @click="toggleDark()">
-            <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--el-menu-item-height)">
-                <i inline-flex i="dark:ep-moon ep-sunny" />
-            </button>
-        </el-menu-item>
-      <div class="flex-grow" />
-      <el-menu-item index="/login" v-if="user.username === ''">登录</el-menu-item>
-      <el-menu-item index="/register" v-if="user.username === ''">注册</el-menu-item>
-      <el-menu-item index="/user" v-if="user.username !== ''">用户中心</el-menu-item>
-      <el-menu-item @click="logout" v-if="user.username !== ''">登出</el-menu-item>
-    </el-menu>
+  <el-menu :default-active="pageIndex" mode="horizontal" class="el-menu-demo" @select="menuSelect" :router=true>
+    <!--        <el-menu-item index="/">主页</el-menu-item>-->
+    <el-menu-item index="/userhome">你好乘客</el-menu-item>
+    <el-menu-item index="/search">车票购买</el-menu-item>
+    <!--        <el-menu-item index="/vipregister">会员注册中心</el-menu-item>
+        <el-menu-item index="/point">积分查询</el-menu-item>-->
+    <el-menu-item index="/admin">切换为管理员页面</el-menu-item>
+    <el-menu-item h="full" @click="toggleDark()">
+      <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--el-menu-item-height)">
+        <i inline-flex i="dark:ep-moon ep-sunny" />
+      </button>
+    </el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="/login" v-if="user.username === ''">登录</el-menu-item>
+    <el-menu-item index="/register" v-if="user.username === ''">注册</el-menu-item>
+    <el-menu-item index="/user" v-if="user.username !== ''">用户中心</el-menu-item>
+    <el-menu-item @click="logout" v-if="user.username !== ''">登出</el-menu-item>
+  </el-menu>
 </template>
 
 <style scoped>
